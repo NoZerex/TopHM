f01bc13bdf9b717cd498a39f3428dca2092dbd0e
NoZerex <nozerex.st@mail.ru>
2025-01-31 23:12:05 +0500

    DBHomeWork 5-6

--- src/DBHomeWork-5-6.sql
+++ src/DBHomeWork-5-6.sql
@@ -7,14 +7,14 @@ CREATE TABLE authors
 
 CREATE TABLE publishers
 (
-    id   BIGSERIAL PRIMARY KEY,
+    id   SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 
 CREATE TABLE books
 (
-    id           BIGSERIAL PRIMARY KEY,
+    id           SERIAL PRIMARY KEY,
     title        VARCHAR(255) NOT NULL,
     publisher_id INT REFERENCES publishers (id) ON DELETE CASCADE
 );
